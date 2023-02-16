ea9427a5e7a1da33eaa94d285dcab392e7d7587d
Suyeol Yun <syyun@mit.edu>
2023-02-16 17:36:13 -0500

    add llmr

--- llmr/gptindex.py
+++ llmr/gptindex.py
@@ -2,28 +2,26 @@ import os
 import dotenv
 
 import utils
-from gpt_index import GPTSimpleVectorIndex
+from gpt_index import GPTSimpleVectorIndex, GPTListIndex, Document
 
 dotenv.load_dotenv("llmr/.env")
 os.environ["OPENAI_API_KEY"] = os.getenv("OPENAI_API_KEY")
 
 
-# text = utils.extract_text_from_pdf(filepath='llmr/pdfs/Matthews1959.pdf')
+text = utils.extract_text_from_pdf(filepath='llmr/pdfs/Matthews1959.pdf')
 # chunks = utils.spliter(text, maxlen=4097)
-
-# from gpt_index import Document
-
 # documents = [Document(t) for t in chunks]
-
+documents = [Document(text)]
 
 # index = GPTSimpleVectorIndex(documents)
+index = GPTListIndex(documents)
 
-# # save to disk
 # index.save_to_disk('index.json')
-# # load from disk
+# index = GPTSimpleVectorIndex.load_from_disk('index.json')
 
-index = GPTSimpleVectorIndex.load_from_disk('index.json')
-print(index.query("Does the apprenticeship could be understood as a type of folkways?"))
+# response = index.query("Summarize the paper including main point of the author.", response_mode="tree_summarize") # good for summary
+response = index.query("What kind of folkways are mentioned in the paper as an example?", response_mode="default") # good for detailed answer
+print(response)
 
 if __name__ == "__main__":
     pass
